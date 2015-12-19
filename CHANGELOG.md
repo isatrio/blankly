c38769cf4f4d360452f198b36dba0b5fd895170a
isatrio <isatrio.is.me@gmail.com>
2015-12-19 08:16:31 +0700

    Add jshint and configuration at Gruntfile.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,50 @@
-### 1.0.3: Desember 12 2015
+# Changelog
+
+## 1.0.4 (Desember 18 2015)
+
+### Changed
+* Changelog format
+* Version to 1.0.4
+* Gruntfile.js configurations
+* package.json add packages
+
+### Added
+* Load grunt tasks
+* Time grunt
+* .jshintrc
+
+
+## 1.0.3 (Desember 12 2015)
+
+### Removed
 * Remove grunt-contrib-less from package.json
+
+### Added
 * Add grunt-sass to package.json
+
+### Fixed
 * Fixing variable on _header.scss
+
+### Changed
 * Change version to 1.0.3
 
 
-### 1.0.2: July 4 2015
+## 1.0.2 (July 4 2015)
+
+### Fixed
 * Fixing Gruntfile, use sass module
 * Fixing Sass file
+
+### Changed
 * Change version to 1.0.2
 
-### 1.0.1: May 31 2015
+
+## 1.0.1 (May 31 2015)
+
+### Changed
 * Change the default grid class
-* Add meta viewport for responsive
 * Change framework, from Bootstrap to Foundation
+
+### Added
+* Add meta viewport for responsive
+
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,79 @@
 'use strict';
 module.exports = function(grunt) {
+
+    // Load all tasks
+    require('load-grunt-tasks')(grunt);
+    // Show elapsed time
+    require('time-grunt')(grunt);
+
     grunt.initConfig({
+        // JS Code Checker
+        jshint: {
+          options: {
+            jshintrc: '.jshintrc'
+          },
+          all: [
+            'Gruntfile.js',
+            'assets/js/*.js',
+            '!assets/**/*.min.*'
+          ]
+        },
+
+        // Compile sass file
         sass: {
-            dist: {
+            dev: {
                 options: {
+                    sourceMap: true,
+                    outputStyle: 'compact'
+                },
+                dist: {
+                    files: {
+                        'assets/css/main.css' : 'assets/scss/main.scss'
+                    }
+                }
+            },
+            prod: {
+                options: {
+                    sourceMap: true,
                     outputStyle: 'compressed'
                 },
-                files: {
-                    'assets/css/main.css' : 'assets/scss/main.scss'
+                dist: {
+                    files: {
+                        'assets/css/main.css' : 'assets/scss/main.scss'
+                    }
                 }
             }
         },
+
+        // Listen file changes
         watch: {
             css: {
                 files: 'assets/scss/*',
-                tasks: ['sass'],
+                tasks: ['sass:dev'],
                 options: {
                     livereload: true
                 }
             },
             js: {
                 files: 'assets/js/main.js',
-                tasks: ['uglify'],
+                tasks: ['jshint', 'uglify'],
                 options: {
                     livereload: true
                 }
             }
         },
+
+        // Compress the js file
         uglify: {
-            build: {
+            prod: {
                 src: 'assets/js/main.js',
                 dest: 'assets/js/main.min.js'
             }
         }
     });
     
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['sass', 'uglify']);
+    // Register task command
+    grunt.registerTask('default', ['sass:dev', 'jshint', 'uglify']);
     grunt.registerTask('dev', ['watch']);
-}
+    grunt.registerTask('production', ['sass:prod', 'jshint', 'uglify']);
+};
